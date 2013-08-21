274b84aacc757b7f1a9bb931fddf2047a37bdf77
wangyzyoga <wangyzyoga@gmail.com>
2013-08-21 16:03:06 +0800

    传递参数

--- 地下城.rb
+++ 地下城.rb
@@ -24,7 +24,7 @@ class Dungeon
     end
     
     def find_room_in_direction(direction)
-      find_room_in_dungeon(@player.location).connections [direction]
+      find_room_in_dungeon(@player.location).connections[direction]
     end
     
     def go(direction)
@@ -64,10 +64,11 @@ my_dungeon.add_room(:largecave,"Large Cave","a large cavernous cave",{ :west =>:
 
 my_dungeon.add_room(:smallcave,"Small Cave","a small,claustrophobic cave",{ :east =>:largecave })
 
-my_dungeon.start(:smallcave)
-
+my_dungeon.start(:largecave)
 
 my_dungeon.go(:west)
+
+my_dungeon.go(:east)
 
 
 
